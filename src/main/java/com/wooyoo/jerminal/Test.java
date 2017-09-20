0397ffe0be247993bdb94dd75e9196c711d62448
kissrain <cjyang1123@gmail.com>
2017-09-20 14:42:28 +0800

    add sleep time to progress bar

--- src/main/java/com/wooyoo/jerminal/Test.java
+++ src/main/java/com/wooyoo/jerminal/Test.java
@@ -1,7 +1,7 @@
 package com.wooyoo.jerminal;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //        System.out.println("\u001b[81mHelloWorld");
         //        System.out.println("哈哈哈哈");
 
@@ -23,6 +23,7 @@ public class Test {
                                             .leftmost()
                                             .content(i + "%")
                                             .build());
+            Thread.sleep(1000);
         }
     }
 }
